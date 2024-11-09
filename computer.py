dab0eebd011ec5027f5aeb0ea3643d4f081e8111
Peace Izere <pizere@smith.edu>
2024-11-08 22:39:07 -0500

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,5 +1,5 @@
 class Computer:
- #attributes
+    # attributes
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -7,31 +7,34 @@ class Computer:
     operating_system: str
     year_made: int
     price: int
-    
-    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int)->None:
-        self.description=description
-        self.processor_type=processor_type
-        self.hard_drive_capacity=hard_drive_capacity
-        self.memory=memory
-        self.operating_system=operating_system
-        self.year_made=year_made
-        self.price=price
-    # The class constructor
+
+    def __init__(self, description: str,
+                 processor_type: str,
+                 hard_drive_capacity: int,
+                 memory: int,
+                 operating_system: str,
+                 year_made: int,
+                 price: int) -> None:
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+
+    # What methods will you need?
     def display(self):
-        print (self.description, self.processor_type, self.hard_drive_capacity, self.memory, self.operating_system, self.year_made, self.price)
-        
-    #Method for updating the price of a computer after resale_shop class checks whether the item is in inventory
-    def update_price(self):
-            new_price=int(input("Enter a new price"))
-            self.price=new_price
-            
-    #updates the os once called by resale_shop class which checks whether the item is inventory
-    def update_os(self):
-            new_operating_system=str(input("Enter the new os"))
-            self.operating_system=new_operating_system
-def main():
-    Mac:Computer = Computer("Mac Pro (Late 2013)","3.5 GHc 6-Core Intel Xeon E5",1024, 64,"macOS Big Sur", 2013, 1500)
-    Mac.update_price()
-    Mac.update_os()
-    Mac.display()
-main()
+        print(f"-" * 2, "New display starts for computer", self.description, "_" * 2)
+        print(self.description, self.processor_type, self.hard_drive_capacity,
+              self.memory, self.operating_system, self.year_made, self.price)
+
+    # Method for updating the price of a computer
+    def update_price(self, new_price: int):
+        # Add an option in the resale shop class to check the item id entered then call update_price option while passing new price in as a float, Else an error message is thrown in resale shop
+        self.price = new_price
+    def updateos(self, new_operating_system: str):
+        if new_operating_system != None:
+            self.operating_system = new_operating_system
+        else:
+            print(f"\nOperating system not specified. There was no updating")
